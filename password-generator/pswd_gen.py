5637a5dd11c70849bf164ad13d02c43641d1f9bc
jmarsh12 <jeffrey.marsh56@gmail.com>
2024-10-26 20:37:36 -0500

    in progress of making it a bit more robust/giving the user options

--- password-generator/pswd_gen.py
+++ password-generator/pswd_gen.py
@@ -1,24 +1,49 @@
 import random
 
 def passwd_prompt():
-    preference = input("Enter S for secure, N for normal: ")
+    usr_input = input("Please enter option, or \"help\" for available options: ")
 
-    if preference == "S":
-        for i in range(9):
-            variables = variables_list[i]
-            print(variables, end="")
-    elif preference == "N":
+    if usr_input == "help":
+        print("==========================================")
+        print("=              Options menu              =")
+        print("= [blank]   -> (default everything)      =")
+        print("= v         -> verbose (generates group) =")
+        print("= s         -> secure                    =")
+        print("= l <value> -> length (default 8, max 20)=")
+
+    # usr_input = input("$ ")
+
+    elif usr_input == "":
         for i in range(7):
             variables = variables_list[i]
             print(variables, end="")
 
-    else:
-        print("Invalid option")
+    elif usr_input == "v":
+        count = 0
+        while count <= 20:
+            for i in range(7):
+                variables = variables_list[i]
+                print(variables, end="")
+            count += 1
+            print("")
+
+    elif usr_input == "s":
+        # TODO: make a separate secure list with a greater variety of capitals and symbols
+        for i in range(7):
+            variables = variables_list[i]
+            print(variables, end="")
+
+    elif usr_input[0] == "l":
+        pass_len = (usr_input[2] + usr_input[3]) # concatenate the value entered for length
+        pass_len = int(pass_len) # cast to an integer to make it an iterator
+        for i in range(pass_len):
+            variables = variables_list[i]
+            print(variables, end="")
 
     print("\n")
-    preference = input("Do you want to generate a new password? Y/N ")
+    usr_input = input("Do you want to generate a new password? Y/N ")
 
-    if preference == "Y" or preference == "y":
+    if usr_input == "Y" or usr_input == "y":
         passwd_prompt()
     else:
         return
@@ -36,5 +61,7 @@ rand_punc = chr(random.randint(33, 38)) # creates a random punctuation point
 variables_list = [up_case1, up_case2, low_case1, low_case2, low_case3, low_case4, rand_int1, rand_int2, rand_punc]
 
 random.shuffle(variables_list)
+
+print("Welcome to Python Password Generator.")
 
 passwd_prompt()
